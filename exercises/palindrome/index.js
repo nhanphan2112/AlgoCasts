8928a923e296582a341362ff45b39242dd5be80d
Nhan Phan <nphanacc@gmail.com>
2021-07-12 19:57:11 -0700

    16-Palindromes solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,33 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+	return str.split("").every((char, i) => {
+		return char === str[str.length - i - 1];
+	});
+}
 
 module.exports = palindrome;
+
+// SOLUTION #1
+// function palindrome(str) {
+// 	let reversed = str.split("").reverse().join("");
+//  return str === reversed;
+// }
+
+// SOLUTION #2
+// function palindrome(str) {
+//   let reversed = "";
+//   for (let characters of str){
+//     reversed = characters + reversed;
+//   }
+//   return str === reversed;
+//  }
+
+
+// SOLUTION #3
+// function palindrome(str) {
+// 	return str.split("").every((char, i) => {
+// 		return char === str[str.length - i - 1];
+// 	});
+// }
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -18,10 +18,38 @@ function reverse(str) {
 	return reversed;
 }
 
+//function only use for debugger
+// reverse('asdf');
+
 module.exports = reverse;
 
-// Solution #1 convert to array (split)
+// SOLUTION #1 convert to array (split)
 // Then (reserve) array and (join) array back into string
 // function reverse(str) {
 // 	return str.split("").reverse().join("");
+// }
+
+// SOLUTION #2
+// function reverse(str) {
+// 	let reversed = "";
+// 	// for (let i = 0; i < str.length; i++) {
+// 	// 	reversed = str[i] + reversed;
+// 	// }
+// 	for (let character of str) {
+// 		reversed = character + reversed;
+// 	}
+
+// 	return reversed;
+// }
+
+
+
+// SOLUTION #3
+// function reverse(str) {
+// 	// reduce is used to take all different values with an arry
+// 	// and condense then all down to one singular value;
+// 	// reduce take 2 argument. First argument is an arrow function
+// 	// second argument is the starting initial value for the function
+//   debugger;
+// 	return str.split("").reduce((rev, char) => char + rev, "");
 // }
