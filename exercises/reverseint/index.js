7519a7364d7ab567125594f5fe6b189a54057901
Nhan Phan <nphanacc@gmail.com>
2021-07-12 20:42:11 -0700

    18-Reversing and Int Solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	const reversed = parseInt(n.toString().split("").reverse().join(""));
+
+	return reversed * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// SOLUTION #1
+// function reverseInt(n) {
+// 	if (Math.sign(n) === 1) {
+// 		return parseInt(n.toString().split("").reverse().join(""));
+// 	} else {
+// 		return 0 - parseInt(Math.abs(n).toString().split("").reverse().join(""));
+// 	}
+// }
+
+// SOLUTION #2
+// function reverseInt(n) {
+// 	const reversed = parseInt(n.toString().split("").reverse().join(""));
+// 	if (n < 0) {
+// 		return reversed * -1;
+// 	}
+// 	return reversed;
+// }
